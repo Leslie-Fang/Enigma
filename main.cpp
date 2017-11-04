34e99c292c021c158d3ad29b523d71bb689b35db
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-11-04 17:23:53 +0800

    fulfill the funcion

--- main.cpp
+++ main.cpp
@@ -10,21 +10,18 @@ char word[26];
 class enigmamachine
 {
 public:
-    vector<int> inpoutPlugboard;
-    vector<int> inpoutreflectors;
-    int numberofRotors;
-    vector<int> inpoutRotorPosition;
+    vector<int> inpoutPlugboard; // mapping the input to the output after plug board
+    vector<int> inpoutreflectors;// mapping the in and out of reflectors
+    int numberofRotors; // store the number of rotors
+    vector<int> inpoutRotorPosition; // store the start position of each rotor
     vector<vector<int> > inpoutRotor;
-    vector<int> norchRotor;
-    vector<vector<int> > mappingRotor;
-    vector<vector<int> > inverseMappingRotor;
-    vector<vector<int> > b2rRotor;
+    vector<int> norchRotor; // store the norch point of each rotor
+    vector<vector<int> > mappingRotor; // mapping the rotor input and output
+    vector<vector<int> > inverseMappingRotor;// inverse mapping the rotor's input and output
+    vector<vector<int> > b2rRotor;// mapping the rotor's relative and absolute frame
 };
 
-
 int main(int argc, char ** argv){
-    cout<<"The number of input params is: "<<argc<<endl;
-    cerr<<"error"<<endl;
     char inputCharacter;
     enigmamachine enigmaMachine;
     string localInput;
@@ -52,8 +49,8 @@ int main(int argc, char ** argv){
     enigmaMachine.inpoutRotor.resize(enigmaMachine.numberofRotors);
     // read all the file and init the enigmaMachine OBJ
     for(int i=1;i<argc;i++){
-        cout<<argv[i]<<endl;
         if(i == 1){
+            // process the plugboards/.pb file and get the inpoutPlugboard mapping
             for(int j=0;j<26;j++){
                 enigmaMachine.inpoutPlugboard.push_back(j);
             }
@@ -109,11 +106,9 @@ int main(int argc, char ** argv){
                 return INCORRECT_NUMBER_OF_PLUGBOARD_PARAMETERS;
             }
             infile.close();
-//            for(int i=0;i<enigmaMachine.inpoutPlugboard.size();i++){
-//                cout<<enigmaMachine.inpoutPlugboard[i]<<endl;
-//            }
         }
         else if(i == 2){
+            // process the reflectors/.rf file and get the reflectors mapping
             ifstream infile(argv[i]);
             if(!infile){
                 cerr<<"ERROR_OPENING_CONFIGURATION_FILE"<<endl;
@@ -167,6 +162,7 @@ int main(int argc, char ** argv){
             }
             infile.close();
         }else if(i == argc-1){
+            // process the rotors/.pos file and get the rotors' init positions
             ifstream infile(argv[i]);
             if(!infile){
                 cerr<<"ERROR_OPENING_CONFIGURATION_FILE"<<endl;
@@ -192,7 +188,7 @@ int main(int argc, char ** argv){
             }
             infile.close();
         }else{
-            cout<<i<<": rotor"<<endl;
+            // process the rotors/.rot file and get the rotors' mapping
             ifstream infile(argv[i]);
             if(!infile){
                 cerr<<"ERROR_OPENING_CONFIGURATION_FILE"<<endl;
@@ -237,7 +233,7 @@ int main(int argc, char ** argv){
         }
         enigmaMachine.norchRotor.push_back(enigmaMachine.inpoutRotor[i][26]);
     }
-    // init the position
+    // init the rotors' position
     enigmaMachine.b2rRotor.resize(enigmaMachine.numberofRotors);
     for(int i=0;i<enigmaMachine.numberofRotors;i++){
         enigmaMachine.b2rRotor[i].resize(26);
@@ -249,7 +245,7 @@ int main(int argc, char ** argv){
             enigmaMachine.b2rRotor[i].erase(enigmaMachine.b2rRotor[i].begin());
         }
     }
-    // init the inverseMappingRotor
+    // init the inverseMappingRotor, when inverse pass the circuit
     enigmaMachine.inverseMappingRotor.resize(enigmaMachine.numberofRotors);
     for(int i=0;i<enigmaMachine.numberofRotors;i++){
         enigmaMachine.inverseMappingRotor[i].resize(26);
@@ -281,13 +277,13 @@ int main(int argc, char ** argv){
 //    for(int i=0;i<enigmaMachine.numberofRotors;i++){
 //        cout<<enigmaMachine.inpoutRotorPosition[i]<<endl;
 //    }
-    cout<<"check the rotor absolute 2 releative position"<<endl;
-    for(int i=0;i<enigmaMachine.numberofRotors;i++){
-        cout<<"rotor: "<<i<<endl;
-        for(int j=0;j<26;j++){
-            cout<<enigmaMachine.b2rRotor[i][j]<<endl;
-        }
-    }
+//    cout<<"check the rotor absolute 2 releative position"<<endl;
+//    for(int i=0;i<enigmaMachine.numberofRotors;i++){
+//        cout<<"rotor: "<<i<<endl;
+//        for(int j=0;j<26;j++){
+//            cout<<enigmaMachine.b2rRotor[i][j]<<endl;
+//        }
+//    }
 
     while(1){
         // press enter; then press ctrl+d ;to terminal the input
@@ -308,14 +304,13 @@ int main(int argc, char ** argv){
             inverseafterRotor.push_back(-1);
         }
         if(inputCharacter){
-            // the charater are not in A-Z
+            // if the charater are not in A-Z, return INVALID_INPUT_CHARACTER error
             if(int(inputCharacter) < 65 || int(inputCharacter) > 90){
                 cerr<<"INVALID_INPUT_CHARACTER"<<endl;
                 return INVALID_INPUT_CHARACTER;
             }
-            //update the mappingRotor before circuit
+            //update the mappingRotor's position before circuit, when enter a character, the rotor would rotor one step
             for(int i=enigmaMachine.numberofRotors-1;i>=0;i--){
-                cout<<enigmaMachine.norchRotor[i]<<endl;
                 if(enigmaMachine.b2rRotor[i][0] == enigmaMachine.norchRotor[i]){
                     enigmaMachine.b2rRotor[i].push_back(enigmaMachine.b2rRotor[i][0]);
                     enigmaMachine.b2rRotor[i].erase(enigmaMachine.b2rRotor[i].begin());
@@ -326,33 +321,12 @@ int main(int argc, char ** argv){
                     break;
                 }
             }
-//            for(int i=enigmaMachine.numberofRotors-1;i>=0;i--){
-//                if(enigmaMachine.b2rRotor[i][25] == enigmaMachine.norchRotor[i]){
-//                    cNum = enigmaMachine.b2rRotor[i][25];
-//                    enigmaMachine.b2rRotor[i].insert(enigmaMachine.b2rRotor[i].begin(),cNum);
-//                    enigmaMachine.b2rRotor[i].pop_back();
-//                    continue;
-//                }else{
-//                    cNum = enigmaMachine.b2rRotor[i][25];
-//                    enigmaMachine.b2rRotor[i].insert(enigmaMachine.b2rRotor[i].begin(),cNum);
-//                    enigmaMachine.b2rRotor[i].pop_back();
-//                    break;
-//                }
-//            }
-
-            cout<<"check the rotor absolute 2 releative position"<<endl;
-            for(int i=0;i<enigmaMachine.numberofRotors;i++){
-                cout<<"rotor: "<<i<<endl;
-                for(int j=0;j<26;j++){
-                    cout<<enigmaMachine.b2rRotor[i][j]<<endl;
-                }
-            }
             cout<<"inputCharacter is: "<<inputCharacter<<" : "<<int(inputCharacter)<<endl;
             // Map character to number: A is 0, Z is 25
             inputNum = int(inputCharacter) - 65;
-            cout<<"inputCharacter is: "<<inputNum<<endl;
+            // pass the enter character to the plug board and get the output of the plugboard
             afterPlugBoard=enigmaMachine.inpoutPlugboard[inputNum];
-            cout<<"afterPlugBoard is: "<<afterPlugBoard<<endl;
+            // pass the character to the rotors
             for(int i=enigmaMachine.numberofRotors-1;i>=0;i--){
                 if(i==enigmaMachine.numberofRotors-1){
                     relativeNumRotor[i] = enigmaMachine.b2rRotor[i][afterPlugBoard];
@@ -361,11 +335,10 @@ int main(int argc, char ** argv){
                     relativeNumRotor[i] = enigmaMachine.b2rRotor[i][afterRotor[i+1]];
                     afterRotor[i] = enigmaMachine.mappingRotor[i][relativeNumRotor[i]];
                 }
-                cout<<"relativeNumRotor"<<i<<" is: "<<relativeNumRotor[i]<<endl;
-                cout<<"afterRotor"<<i<<" is: "<<afterRotor[i]<<endl;
             }
+            // pass the character to the reflectors
             afterReflector = enigmaMachine.inpoutreflectors[afterRotor[0]];
-            cout<<"afterReflector is: "<<afterReflector<<endl;
+            // pass the character inverse to the rotors
             for(int i=0;i<enigmaMachine.numberofRotors;i++){
                 if(i == 0){
                     inverseafterRotor[i] = enigmaMachine.inverseMappingRotor[i][afterReflector];
@@ -383,8 +356,8 @@ int main(int argc, char ** argv){
                     }
                 }
             }
+            // pass the character inverse to the plug board and get the finally output
             afterPlugBoardlast = enigmaMachine.inpoutPlugboard[inverserelativeNumRotor[enigmaMachine.numberofRotors-1]];
-            cout<<"afterPlugBoardlast is: "<<afterPlugBoardlast<<endl;
             cout<<"The output character is: "<<char(afterPlugBoardlast+65)<<endl;
         }else{
             break;
